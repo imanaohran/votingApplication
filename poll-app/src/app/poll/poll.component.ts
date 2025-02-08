7c8188a13585a29dc2cf3089f0923b7367f21d88
Imana Ohran <imana.ohran.20@size.ba>
2025-02-08 22:03:36 +0100

    feat: Enable create polls

--- poll-app/src/app/poll/poll.component.ts
+++ poll-app/src/app/poll/poll.component.ts
@@ -13,6 +13,15 @@ import { FormsModule } from '@angular/forms';
 })
 export class PollComponent implements OnInit {
 
+  newPoll: Poll = { 
+    id: 0, 
+    question: '',
+    options: [
+      {optionText: '', voteCount: 0},
+      {optionText: '', voteCount: 0}
+    ]
+  }
+
   polls: Poll[] = [];
   
   constructor(private pollService: PollService) { }
@@ -26,5 +35,17 @@ export class PollComponent implements OnInit {
       next: (data) => {this.polls = data;},
       error: (error) => {console.error('There is an error!', error)}
     });
+  }
+
+  createPoll() {
+    this.pollService.createPoll(this.newPoll).subscribe({
+      next: (createdPoll) => {
+        this.polls.push(createdPoll);},
+      error: (error) => {console.error('There is an error!', error)}
+    })
+  }
+
+  trackByIndex(index: number): number{
+    return index;
   }
 }
--- src/main/java/com/voting/voting_app/model/OptionVote.java
+++ src/main/java/com/voting/voting_app/model/OptionVote.java
@@ -9,6 +9,6 @@ import lombok.NoArgsConstructor;
 @Embeddable
 public class OptionVote {
 
-    private String optionVote;
+    private String optionText;
     private Long voteCount = 0L;
 }
